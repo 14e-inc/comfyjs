7e8e0ebc5ad42390ed9ce6f06a271d6b652d5d74
Hebron Watson <hebronwatson@gmail.com>
2026-06-10 10:05:53 -0400

    change dotenv import

--- packages/comfy-auth-util/vitest.config.js
+++ packages/comfy-auth-util/vitest.config.js
@@ -4,7 +4,7 @@
 // Configure Vitest (https://vitest.dev/config/)
 
 import { defineConfig } from 'vite'
-import { dotenv } from 'dotenv'
+import 'dotenv/config'
 
 export default defineConfig({
   test: {
